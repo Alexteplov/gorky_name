14e89cc7af6424edc7a4082160474f5c55f38845
unknown <teplov0@gmail.com>
2022-04-14 14:05:28 +0500

    add localization

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:gorky_name/const/const_variables.dart';
+import 'package:gorky_name/generated/l10n.dart';
 import 'package:gorky_name/internal/theme/custom_theme.dart';
 import 'package:gorky_name/ui/screens/main_screen.dart';
 
@@ -14,6 +16,13 @@ class _ApplicationState extends State<Application> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
       title: GlobalVar.nameLib,
       theme: CustomTheme.lightTheme,
